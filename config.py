c7ff37facb13ac40e704856efc8ae63dc50754f5
ngleewen <163620244+ngleewen@users.noreply.github.com>
2024-04-19 11:37:30 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.c5yqkiwyqzin.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "ngleewen-employee"
+customregion = "us-east-1"
+
